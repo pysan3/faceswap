d6c433ea9278e598c325e9b6dfd5ccde72ad3788
Takuto <pysan3@gmail.com>
2019-09-29 12:15:41 +0900

    hoge

--- get_data.sh
+++ get_data.sh
@@ -3,4 +3,3 @@ ffmpeg -i he/scene.mp4 -vf fps=30 he/scene%06d.png
 python img_collector.py 橋本環奈 10000
 python faceswap.py extract -i he -o hisface
 python faceswap.py extract -i me -o myface
-python faceswap.py train -A hisface -B myface -m models -p
