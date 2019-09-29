96c22ebbedda9aed6afdc4e44625766971df3687
Takuto <pysan3@gmail.com>
2019-09-29 18:12:06 +0900

    hoge

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 ffmpeg -i he/hoge.mp4 -vf fps=30 he/hoge%06d.png
-python img_collector.py -t 橋本環奈 -n 10000
+python img_collector.py -t hashimotokanna -n 10000
 python faceswap.py extract -i he -o hisface
 python faceswap.py extract -i me -o myface
 python faceswap.py train -A hisface -B myface -m models -p
