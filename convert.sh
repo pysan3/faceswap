7286e842ee39ade35efa89c1aa94e68fd277fb66
Takuto <pysan3@gmail.com>
2019-09-29 17:46:32 +0900

    hoge

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 python faceswap.py convert -i he -o output -m models
-ffmpeg -r 30 -i output/scene%06d.png -c:v libx264 -pix_fmt yuv420p out.mp4
+ffmpeg -r 30 -i output/hoge%06d.png -c:v libx264 -pix_fmt yuv420p out.mp4
